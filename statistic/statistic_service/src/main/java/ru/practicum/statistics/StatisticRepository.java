5515e041332252a642438fed5b89f5c70e5a4789
Max-Jordan <max19992319@yandex.ru>
2023-03-30 14:17:00 +0300

    add

--- statistic/statistic_service/src/main/java/ru/practicum/statistics/StatisticRepository.java
+++ statistic/statistic_service/src/main/java/ru/practicum/statistics/StatisticRepository.java
@@ -10,28 +10,28 @@ import java.util.List;
 
 public interface StatisticRepository extends JpaRepository<Log, Long> {
 
-    @Query("select new ru.practicum.statistics.model.LogResp(l.app, l.uri, count(l.ip)) "
+    @Query("select new ru.practicum.statistics.model.ViewStat(l.app, l.uri, count(l.ip)) "
             + " from Log l "
             + " where l.timestamp between ?1 and ?2 "
             + " group by l.app, l.uri "
             + " order by count(l.ip) desc")
     List<ViewStat> findAllStatisticWithoutUris(LocalDateTime start, LocalDateTime end);
 
-    @Query("select new ru.practicum.statistics.model.LogResp(l.app, l.uri, count(distinct l.ip)) "
+    @Query("select new ru.practicum.statistics.model.ViewStat(l.app, l.uri, count(distinct l.ip)) "
             + " from Log l "
             + " where l.timestamp between ?1 and ?2 "
             + " group by l.app, l.uri"
             + " order by count(distinct l.ip) desc")
     List<ViewStat> findAllUniqueStatisticsWithoutUris(LocalDateTime start, LocalDateTime end);
 
-    @Query("select new ru.practicum.statistics.model.LogResp(l.app, l.uri, count(l.ip)) "
+    @Query("select new ru.practicum.statistics.model.ViewStat(l.app, l.uri, count(l.ip)) "
             + " from Log l "
             + " where l.uri in (?3) and l.timestamp between ?1 and ?2 "
             + " group by l.app, l.uri "
             + " order by count(l.ip) desc")
     List<ViewStat> findAllStatistics(LocalDateTime start, LocalDateTime end, List<String> uris);
 
-    @Query("select new ru.practicum.statistics.model.LogResp(l.app, l.uri, count(distinct l.ip)) "
+    @Query("select new ru.practicum.statistics.model.ViewStat(l.app, l.uri, count(distinct l.ip)) "
             + " from Log l "
             + " where l.uri in (?3) and l.timestamp between ?1 and ?2 "
             + " group by l.app, l.uri"
