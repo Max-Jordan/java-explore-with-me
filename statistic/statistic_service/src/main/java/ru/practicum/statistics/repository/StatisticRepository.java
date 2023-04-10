38e53a97b6a31c09b7595d2319849812d13bff4e
Max-Jordan <max19992319@yandex.ru>
2023-04-10 23:03:12 +0500

    imp

--- statistic/statistic_service/src/main/java/ru/practicum/statistics/repository/StatisticRepository.java
+++ statistic/statistic_service/src/main/java/ru/practicum/statistics/repository/StatisticRepository.java
@@ -1,12 +1,7 @@
 package ru.practicum.statistics.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import ru.practicum.statistics.model.EndpointHit;
-import ru.practicum.statistics.model.ViewStat;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 public interface StatisticRepository extends JpaRepository<EndpointHit, Long>, CustomStatisticRepository {
 }
